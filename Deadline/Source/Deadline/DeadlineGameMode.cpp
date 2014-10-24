4618972950aae8307b211c0a528b45aebdacf2be
Boomer <herman.rogers@gmail.com>
2014-10-24 20:14:07 +0100

    Adding intial project files

---
+++ Deadline/Source/Deadline/DeadlineGameMode.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Deadline.h"
+#include "DeadlineGameMode.h"
+
+
+ADeadlineGameMode::ADeadlineGameMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
---
+++ Deadline/Source/Deadline/DeadlineGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DeadlineGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEADLINE_API ADeadlineGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
