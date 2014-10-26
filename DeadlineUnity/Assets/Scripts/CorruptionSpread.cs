5bed67c239a759656b5f995715aae51c1a49e8b6
Boomer <herman.rogers@gmail.com>
2014-10-26 10:19:10 +0000

    Fixixing unityvs merge conflict

---
+++ DeadlineUnity/Assets/Scripts/CorruptionSpread.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CorruptionSpread : MonoBehaviour
+{
+    public int gridSize;
+    private Vector3 blockSize = new Vector3( 1,1,1 );
+    private Vector3 movementDirection = new Vector3( 0,0,0 );
+
+    void Start( )
+    {
+        Create2DGrid( );
+    }
+
+    private void Create2DGrid( )
+    {
+        for ( int i = 0; i < gridSize; i++ )
+        {
+            Vector3 newGridPosition = transform.position + movementDirection;
+            newGridPosition = new Vector3( Mathf.Round( newGridPosition.x / blockSize.x ),
+                                           Mathf.Round( newGridPosition.y / blockSize.y ),
+                                           Mathf.Round( newGridPosition.z / blockSize.z ) );
+            transform.position = newGridPosition;
+        }
+    }
+}
--- DeadlineUnity/Assets/Scripts/GameManager.cs
+++ DeadlineUnity/Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour {
 		Camera.main.clearFlags = CameraClearFlags.Skybox;
 		Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
 		mazeInstance = Instantiate(mazePrefab) as Maze;
-		yield return StartCoroutine(mazeInstance.Generate());
+        yield return StartCoroutine( mazeInstance.Generate( ) );
 		playerInstance = Instantiate(playerPrefab) as Player;
 		playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
 		Camera.main.clearFlags = CameraClearFlags.Depth;
